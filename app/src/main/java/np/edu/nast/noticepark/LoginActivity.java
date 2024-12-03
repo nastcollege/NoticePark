97e7f46f3354f9b7d62edad1e73f0ff79331d2bd
nastcollege <nastcollegegit@gmail.com>
2024-12-03 12:18:06 +0545

    Login Page and button event handling

--- app/src/main/java/np/edu/nast/noticepark/LoginActivity.java
+++ app/src/main/java/np/edu/nast/noticepark/LoginActivity.java
@@ -1,6 +1,10 @@
 package np.edu.nast.noticepark;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +14,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText etUsername;
+    private EditText etPassword;
+
+    private Button btnLogin;
+
+    private Button btnSignup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnLogin.setOnClickListener(view -> {
+            Toast.makeText(getApplicationContext(),"Clicked Me!!",Toast.LENGTH_LONG).show();
+        });
 
     }
 }
