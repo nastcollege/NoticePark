211a42543333020eb2c4f32f8306514bdb9f5689
nastcollege <nastcollegegit@gmail.com>
2025-01-12 12:15:28 +0545

    Wev View Example

--- app/src/main/java/np/edu/nast/noticepark/WebViewActivity.java
+++ app/src/main/java/np/edu/nast/noticepark/WebViewActivity.java
@@ -1,9 +1,12 @@
 package np.edu.nast.noticepark;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,7 +26,19 @@ private WebView webView;
         WebSettings settings = webView.getSettings();
         settings.getJavaScriptEnabled();
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://nast.edu.np/");
+        if(isOnline()) {
+            webView.loadUrl("https://nast.edu.np/");
+        }else{
+            Toast.makeText(getApplicationContext(),"NO Internet Avaliable",Toast.LENGTH_LONG).show();
+        }
 
     }
+
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null &&
+                cm.getActiveNetworkInfo().isConnectedOrConnecting();
+    }
+
 }
