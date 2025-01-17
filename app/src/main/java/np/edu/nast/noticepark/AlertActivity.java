c87a1c9f38696d6d54fc1bba8c5c9f796743723b
nastcollege <nastcollegegit@gmail.com>
2025-01-17 12:31:06 +0545

    Custom Toast Example

--- app/src/main/java/np/edu/nast/noticepark/AlertActivity.java
+++ app/src/main/java/np/edu/nast/noticepark/AlertActivity.java
@@ -1,7 +1,11 @@
 package np.edu.nast.noticepark;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -13,19 +17,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AlertActivity extends AppCompatActivity {
-private Button btnAlert;
+    private Button btnAlert;
+    private Button btnCustomToast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert);
 
-        btnAlert=findViewById(R.id.btnAlert);
-        btnAlert.setOnClickListener(view -> {
+        btnAlert = findViewById(R.id.btnAlert);
+        btnAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnAlert.setOnClickListener((view) -> {
             AlertDialog.Builder alert = new AlertDialog.Builder(AlertActivity.this);
             alert.setMessage("Login Success");
             alert.setTitle("This is title Page!");
             alert.setIcon(R.drawable.alert);
             alert.setCancelable(true);
+
             alert.setPositiveButton("OK", (dialog, which) -> {
                 Toast.makeText(AlertActivity.this, "OK Clicked",
                         Toast.LENGTH_LONG).show();
@@ -35,6 +48,24 @@ private Button btnAlert;
                         Toast.LENGTH_LONG).show();
             });
             alert.show();
+        });
+
+        btnCustomToast = findViewById(R.id.btnCustomToast);
+        btnCustomToast.setOnClickListener(view -> {
+
+            //Creating the LayoutInflater instance
+            LayoutInflater li = getLayoutInflater();
+
+            //Getting the View object as defined in the customtoast.xml file
+            View layout = li.inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.customToastLayout));
+
+            //Creating the Toast object
+            Toast toast = new Toast(getApplicationContext());
+            toast.setDuration(Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.setView(layout);//setting the view of custom toast layout
+            toast.show();
+
         });
 
     }
