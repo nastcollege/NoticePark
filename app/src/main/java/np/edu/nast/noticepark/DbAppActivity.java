fa50734d807cb36c25238b8e22c0811ce1fe05f4
nastcollege <nastcollegegit@gmail.com>
2025-01-26 07:57:29 +0545

    DB Crud finilize

--- app/src/main/java/np/edu/nast/noticepark/DbAppActivity.java
+++ app/src/main/java/np/edu/nast/noticepark/DbAppActivity.java
@@ -1,6 +1,11 @@
 package np.edu.nast.noticepark;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,70 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DbAppActivity extends AppCompatActivity {
+    private MyDbOpenHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_db_app);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        db = new MyDbOpenHelper(this);
+        Button btnCreate = findViewById(R.id.createRecord);
+        final Button btnRead = findViewById(R.id.readRecord);
+        final Button btnUpdate = findViewById(R.id.btnUpdate);
+        final Button btnDelete = findViewById(R.id.btnDelete);
+        final EditText etId = findViewById(R.id.etId);
+        final EditText etFirstName = findViewById(R.id.et_first_name);
+        final EditText etLastName = findViewById(R.id.et_last_name);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (db.insert(etFirstName.getText().toString(),
+                        etLastName.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "Record Saved", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Unable to Save  Record", Toast.LENGTH_LONG).show();
+                }
+            }
         });
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tvDbResult = findViewById(R.id.db_result);
+                StringBuffer buffer = new StringBuffer();
+                for (String s : db.readAll()) {
+                    buffer.append(s + "\n");
+                }
+                tvDbResult.setText(buffer);
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer id = Integer.parseInt(etId.getText().toString());
+                if (db.updateRecord(id, etFirstName.getText().toString(), etLastName.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "Updated!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Sorry", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer id = Integer.parseInt(etId.getText().toString());
+                if (db.deleteRecord(id)) {
+                    Toast.makeText(getApplicationContext(), "Deleted", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Sorry", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/np/edu/nast/noticepark/MyDbOpenHelper.java
+++ app/src/main/java/np/edu/nast/noticepark/MyDbOpenHelper.java
@@ -1,4 +1,97 @@
 package np.edu.nast.noticepark;
 
-public class MyDbOpenHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyDbOpenHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "nast.db";
+    public static final String DB_LOG = "DB";
+    private static final int DB_VERSION = 1;
+    private static final String STUDENT_TABLE_NAME = "tbl_students";
+    private static final String ID = "id";
+    private static final String FIRST_NAME = "first_name";
+    private static final String LAST_NAME = "last_name";
+    private static final String STUDENT_TABLE_SQL = "CREATE TABLE " +
+            STUDENT_TABLE_NAME + "(" +
+            ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            FIRST_NAME + " TEXT, " +
+            LAST_NAME + " TEXT)";
+
+    public MyDbOpenHelper(Context ctx) {
+        super(ctx, DB_NAME, null, DB_VERSION);
+        Toast.makeText(ctx, "Created", Toast.LENGTH_LONG).show();
+        Log.i(DB_LOG, "Database Created Successfully");
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i(DB_LOG, STUDENT_TABLE_SQL);
+        db.execSQL(STUDENT_TABLE_SQL);
+        Log.i(DB_LOG, "Table Created Successfully");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        String sql = "DROP TABLE IF EXISTS " + STUDENT_TABLE_NAME;
+        db.execSQL(sql);
+        onCreate(db);
+
+    }
+
+    public boolean insert(String firstName, String lastName) {
+        ContentValues values = new ContentValues();
+        values.put(FIRST_NAME, firstName);
+        values.put(LAST_NAME, lastName);
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        boolean ins = db.insert(STUDENT_TABLE_NAME, null, values) > 0;
+        Log.i(DB_LOG, "Inserted");
+        db.close();
+        return ins;
+    }
+
+    public List<String> readAll() {
+        SQLiteDatabase db = getReadableDatabase();
+        String sql = "SELECT * FROM " + STUDENT_TABLE_NAME;
+        List<String> students = new ArrayList<>();
+        Cursor cursor = db.rawQuery(sql, null);
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                String id = cursor.getString(cursor.getColumnIndexOrThrow(ID));
+                String firstName = cursor.getString(cursor.getColumnIndexOrThrow(FIRST_NAME));
+                String lastName = cursor.getString(cursor.getColumnIndexOrThrow(LAST_NAME));
+                String student = id + ": " + firstName + " " + lastName;
+                students.add(student);
+            } while (cursor.moveToNext());
+        }
+        db.close();
+        return students;
+    }
+
+    public boolean updateRecord(long id, String firstName, String lastName) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues args = new ContentValues();
+        args.put(ID, id);
+        args.put(FIRST_NAME, firstName);
+        args.put(LAST_NAME, lastName);
+        int i = db.update(STUDENT_TABLE_NAME, args, ID + "=" + id, null);
+        return i > 0;
+    }
+
+    public boolean deleteRecord(Integer id) {
+        SQLiteDatabase db = getWritableDatabase();
+        // db.execSQL("DELETE FROM " + STUDENT_TABLE_NAME+ " WHERE "+ID+"='"+id+"'");
+        return db.delete(STUDENT_TABLE_NAME, ID + "=" + id, null) > 0;
+        //
+    }
 }
+
+
